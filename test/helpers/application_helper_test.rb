ba19e632a8d1d4fb1bf3fde722d6ee2057e696ab
RishabhKhatri <rishabh15077@iiitd.ac.in>
2016-07-03 02:44:00 +0530

    Layout changes

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class ApplicationHelperTest < ActionView::TestCase
   test "full title helper" do
-    assert_equal full_title,         "Ruby on Rails Tutorial Sample App"
-    assert_equal full_title("Help"), "Help | Ruby on Rails Tutorial Sample App"
+    assert_equal full_title,         "Majestic Ads"
+    assert_equal full_title("Help"), "Help | Majestic Ads"
   end
 end
